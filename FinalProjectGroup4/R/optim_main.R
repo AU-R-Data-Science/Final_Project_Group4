e1a45ea6ca448800f6e2b84e7adad91f81381807
Mauren Baker <89655300+sCoOTtter@users.noreply.github.com>
2022-11-28 14:51:32 -0600

    Update optim_main.R

--- FinalProjectGroup4/R/optim_main.R
+++ FinalProjectGroup4/R/optim_main.R
@@ -1,11 +1,23 @@
-least_squares <- function(response, predictors)
+least_squares <- function(response, predictors, alpha)
 {
   response <- as.vector(response)
   predictors <- as.matrix(predictors)
 
+  n <- length(response)
+  p <- dim(predictors)[2]
+  df <- n - p
+
   inv_pred <- solve(t(predictors) %*% predictors)
   beta <- inv_pred %*% t(predictors) %*% response
-  return(beta)
+
+  residual <- response - predictors %*% as.matrix(beta)
+  sig2 <- (1/df) * t(residual) %*% residual
+  var_b <- sig2 * inv_pred
+
+  quantile <- 1 - alpha/2
+  ci <- c(beta - qnorm(p = quantile) * sqrt(var_b), beta + qnorm(p = quantile) * sqrt(var_b))
+
+  return(list(beta = beta, sigma2 = sig2, variance_beta = var_b, confidence_interval = ci))
 }
 
 optimize <- function(response, predictors, beta)
@@ -16,22 +28,17 @@ optimize <- function(response, predictors, beta)
     }
 }
 
-bootstrap_ci <- function()
-{
-  
-}
-
 conf_matrix <- function()
 {
-  
+
 }
 
 log_curve <- function()
 {
-  
+
 }
 
 plot_metrics <- function()
 {
-  
+
 }
