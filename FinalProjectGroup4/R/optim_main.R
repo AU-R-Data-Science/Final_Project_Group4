b49a292a64f53d98d4e306400d64b34d3875d135
Micheal Jackson <msj0028@auburn.edu>
2022-12-01 00:10:14 -0600

    Confusion Matrix Completed
    I also added some stuff for CI Bootstrap, but didn't finish.

--- FinalProjectGroup4/R/optim_main.R
+++ FinalProjectGroup4/R/optim_main.R
@@ -51,19 +51,52 @@ optimize <- function(y, X)
 
 bootstrap_ci <- function(alpha, rounds = 20)
 {
-  beta_mat <- matrix(NA, B, )
-    #for (b in 1:rounds)
-    #{
-    # boot_data <-
-    # beta_mat[b, ] <- lm()$coefficients
-    #}
+  beta_mat <- matrix(NA, B, 2)
+    for (b in 1:rounds)
+    {
+     boot_data <- data[sample(1:nrows(data), nrows(data), replace = TRUE), ]
+     beta_mat[b, ] <- lm(y~x, data = boot_data)$coefficients
+    }
 
   return(list(bootstrap_ci = boot_ci))
 }
 
-  conf_matrix <- function()
+#' Create a confusion matrix and output its metrics
+#' @description This function provides and uses a confusion matrix to assess how closely an optimization technique models the data presented.
+#' @param y A \code{double} value of the vector containing the response of interest.
+#' @param X An \eqn{n \times p} \code{double} value of the matrix containing the values of the predictors.
+#' @param cutoff A \code{double} value that sets the probability which determines the predicticted classification of the data set.
+#' @return A \code{list} containing the following objects:
+#' \describe{
+#'  \item{Prevalence}{The percentage of positive cases in the data set}
+#'  \item{Accuracy}{The percentage of cases predicted correctly by the model}
+#'  \item{Sensitivity}{The percentage of positive cases predicted correctly by the model}
+#'  \item{Specificity}{The percentage of negative cases predicted correctly by the model}
+#'  \item{False Discovery Rate}{The percentage of cases predicted positive by the model that are actually negative}
+#'  \item{Diagnostic Odds Ratio}{The ratio of the probability of a positive predicted as a negative to the probability of a negative predicted as a negative}
+#' }
+#' @author Micheal Stewart Jackson
+#' @export
+  conf_matrix <- function(y, X, cutoff)
   {
-
+  optomize(y, X)
+    predict <- ifelse(beta_est > cutoff, yes = 1, no = 0)
+  cm <- table(y, predict) # confusion matrix
+  
+  tp <- cm[1, 1] # Number of true positives
+  fn <- cm[1, 2] # Number of false negatives
+  fp <- cm[2, 1] # Number of false positives
+  tn <- cm[2, 2] # Number of true negatives
+  
+  prev <- (tp+fp)/(tp+fp+fn+tn)
+  acc <- (tp+tn)/(tp+fp+tn+fn)
+  tpr <- tp/(tp+fn)
+  tnr <- tn/(tn+fp)
+  fdr <- fp/(fp+tp)
+  dor <- (tpr/(1-tnr))/((1-tpr)/tnr)
+  
+  
+return(list("Prevalence" = prev, "Accuracy" = acc, "Sensitivity" = tpr, "Specificity" = tnr, "False Discovery Rate" = fdr, "Diagnostic Odds Ratio" = dor))
   }
 
   log_curve <- function()
